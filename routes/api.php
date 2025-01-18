efa593e36e4ae8fb003269de543ae3f23c30649c
mostafaniakan <mostafaniakan96@gmail.com>
2025-01-18 13:31:01 +0330

    Config Redis

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 Route::prefix('user')->group(function () {
     Route::post('store', [UserController::class, 'store']);
 });
